efabe804be3cde4f205efc4b7c3487eeba36b551
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2025-09-26 10:00:41 +0300

    Save

--- Editor.js
+++ Editor.js
@@ -1,11 +1,62 @@
 /*
-CF = (C * 1.8) + 32
-CK = K = C + 273.15
-FC = C = (F - 32) % 1.8
-FK = K = (F + 459.67) * 5/9
-KC = C = K - 273.15
-KF = F = (K * 9/5) - 459.67
+CF = F = (C * 1.8) + 32 (0)
+CK = K = C + 273.15 (0)
+FC = C = (F - 32) / 1.8 (−273,15)
+FK = K = (F + 459.67) * 5/9 (0)
+KC = C = K - 273.15 (−273,15)
+KF = F = (K * 9/5) - 459.67 (0)
 */
-ToggleEventListener("D1", "D2", "D3");
-document.getElementById("convertBtn").addEventListener("click", Convert);
 
+function convertTemperature() {
+    let input = document.getElementById("määrä").value.trim();
+
+    if (input === "") {
+        console.log("Tyhjää muunnosta ei sallita");
+        return;
+    }
+
+    let number = parseFloat(input);
+
+    if (isNaN(number)) {
+        console.log("Merkkien antaminen ei ole sallittua");
+        return;
+    }
+
+    let muunnos = document.getElementById("valinta").value;
+    let desimaalit = document.querySelector('input[name="desimaali"]:checked').value;
+
+    let palautus;
+
+    switch (muunnos) {
+        case "CF":
+            palautus = number * 9 / 5 + 32;
+            palautus = palautus.toFixed(desimaalit) + "°F";
+            break;
+        case "CK":
+            palautus = number + 273.15;
+            palautus = palautus.toFixed(desimaalit) + "K";
+            break;
+        case "FC":
+            palautus = (number - 32) * 5 / 9;
+            palautus = palautus.toFixed(desimaalit) + "°C";
+            break;
+        case "FK":
+            palautus = (number - 32) * 5 / 9 + 273.15;
+            palautus = palautus.toFixed(desimaalit) + "K";
+            break;
+        case "KC":
+            palautus = number - 273.15;
+            palautus = palautus.toFixed(desimaalit) + "°C";
+            break;
+        case "KF":
+            palautus = (number - 273.15) * 9 / 5 + 32;
+            palautus = palautus.toFixed(desimaalit) + "°F";
+            break;
+        
+    }
+    if (palautus < 0) {
+            console.log("Lämpötila on negatiivinen");
+            return;
+        }
+    document.getElementById("output").innerText = palautus;
+}
